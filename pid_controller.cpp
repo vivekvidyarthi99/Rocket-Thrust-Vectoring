8e26b647f2af0c576f1223c6c18954516d50e665
Vivek Vidyarthi <vivek@ArchLinux.localdomain>
2018-12-25 23:43:41 -0500

    PID Control Initialization

--- pid_controller.cpp
+++ pid_controller.cpp
@@ -5,31 +5,72 @@
 
 
 
-#include "pid_controller.h"
-
-
-int k_p = 1;
-int k_i = 1;
-int k_d = 1;
-
-/**
- * k_p = proportional constant
- * k_i = integrational constant
- * k_d = differential constant
- * err = expected output - actual output (error)
- * integ = integral error from previous loop + err (integral error)
- * der = erro - err from previous loop (differential error)
- * dt = execution time of loop
- **/
-
-int err = 0;
-int integ = 0;
-int der = 0;
-int dt = 0;
-
-int t = 700;
-
-while(t>0){
-	output = k_p +err + (k_i * integ * dt) + (k_d * der / dt);
-	t--;
+PID::PID()
+{
+	kp = 0;
+	ki = 0;
+	kd = 0;
+	err = 0;
+	sum_err = 0;
+	dif_err = 0;
+	lastInput = 0;
+	outmax = 400;
+	outmin = -400;
 }
+
+PID::PID(float kp_, float ki_, float kd_)
+{
+	kp = kp_;
+	ki = ki_;
+	kd = kd_;
+
+	err = 0;
+	sum_err = 0;
+	dif_err = 0;
+	lastInput = 0;
+	outmax = 400;
+	outmin = -400;
+}
+
+float PID::update_pid_std(float setpoint, float input, float dt)
+{
+	err = setpoint-input;
+	sum_err += err*ki*dt;
+	dif_err = -kd/dt * (input - lastInput);
+	output = kp*err + sum_err + dif_err;
+	if (output > outmax) {
+		sum_err = 0.0;
+		output = outmax;
+	}else if (output < outmin) {
+		sum_err =0.0;
+		output = outmin;
+	}
+	lastInput = input;
+	return output;
+
+}
+
+
+void PID::reset()
+{
+	sum_err = 0;
+	dif_err = 0;
+	lastInput = 0;
+}
+
+
+void PID::set_Kpid(float KP, float KI, float KD)
+{
+	kp = KP;
+	ki = KI;
+	kd = KD;
+}
+void PID::set_windup_bounds(float min, float max)
+{
+	outmax = max;
+	outmin = min;
+}
+
+
+	
+
--- pid_controller.h
+++ pid_controller.h
@@ -1,5 +1,38 @@
-/**
- * Header Files
- **/
+#ifndef pid_controller
+#define pid_controller
 
+
+#include <iostream>
+#include <stdlib.h>
+#include <fstream>
+
+
+class PID {
+
+	private:
+		float kp;
+		float ki;
+		float kd;
+
+		float err;
+		float sum_err;
+		float dif_err;
+		float lastInput;
+		float max;
+		float min;
+		float output;
+
+	public:
+		PID();
+		PID(float,float,float);
+		float update_pid_std(float setpt, float input, float dt);
+		void setKpid(float, float, float);
+		void set_windup_bounds(float, float);
+		void reset();
+		float setpoint;
+
+};
+
+
+#endif
 
